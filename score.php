b43b4c27352d5dd10d520f90427754b6c2cc770b
Ramunas Andrijauskas <ramunas.andrijauskas@intermedix.com>
2018-01-10 13:14:17 +0200

    added new inputs

--- score.php
+++ score.php
@@ -5,15 +5,24 @@
  * Date: 10/20/2017
  * Time: 3:59 PM
  */
+// vars from input
 $people = $_GET["people"];
 $project = $_GET["project"];
+$testing = $_GET["testing"];
+$budget = $_GET["budget"];
+$story = $_GET["story"];
 $project_type = $_GET["project_type"];
-$investmentAmount = '20%';
-$sroi = '17%';
+// temp
+$sroi = '17';
+$breakEven = '90';
+$ratio = '1.25';
 $estPrice = '0.99';
 
 $pattern = "You have %s people working.
 <br>Your project have %s working days in total
+<br>Your project have %s of project working days dedicated to testing
+<br>Your budget to spend on testing is %s %% from all the project budget
+<br>Your estimated number of user stories in project is %s
 <br>Your project type is: %s";
 
 switch($project_type){
@@ -25,7 +34,16 @@ switch($project_type){
         break;
 }
 
-?> <!<!doctype html>
+switch($project_type){
+    case $project_type === "commercial_project":
+        $project_type = "Komercinis projektas";
+        break;
+    case $project_type === "internal_project":
+        $project_type = "Vidinis projektas";
+        break;
+}
+
+?> <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -34,15 +52,17 @@ switch($project_type){
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>MATS Application</title>
     <h5>
-<?php print(sprintf($pattern, $people, $project, $project_type)); ?>
+<?php print(sprintf($pattern, $people, $project, $testing, $budget, $story, $project_type));?>
     </h5>
     <hr>
 </head>
 <body>
 <table>
 <?php
-print ("<tr><td><b>Investment in product quality: </b></td><td><p>{$investmentAmount}</p></td></tr>
-<tr><td><b>Estimated Social Return on Investment: </b></td><td><p>{$sroi}</p></td></tr>");
+print ("<tr><td><b>Investment in product quality: </b></td><td><p>{$budget} %</p></td></tr>
+<tr><td><b>Estimated Social Return on Investment: </b></td><td><p>{$sroi} %</p></td></tr>
+<tr><td><b>Estimated break even period: </b></td><td><p>{$breakEven} days</p></td></tr>
+<tr><td><b>Resource cost ratio (Manual vs Automated) </b></td><td><p>{$ratio} : 1</p></td></tr>");
 ?>
 </table>
 <hr>
